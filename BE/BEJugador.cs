6c53c416de2a240ab97d5b715f453143a2a7e22e
Fraanlnc <97235905+Fraanlnc@users.noreply.github.com>
2022-02-07 10:52:21 -0300

    Add files via upload

---
+++ BE/BEJugador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class BEJugador
+    {
+        public BEJugador()
+        {
+
+
+        }
+
+        public int Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int DNI { get; set; }
+
+        public string Mail { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Localidad { get; set; }
+
+        public override string ToString()
+        {
+            return this.Codigo + " " + this.Nombre + " " + this.Apellido;
+        }
+    }
+}
---
+++ BE/BEPartidas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class BEPartidas
+    {
+        public BEPartidas()
+        {
+
+        }
+
+       // public int Codigo { get; set; }
+
+        public BEJuego Juego { get; set; }
+        public BEJugador Jugador { get; set; }
+        
+        public int Ganadas { get; set; }
+        public int Perdidas { get; set; }
+        public int Empates { get; set; }
+
+        public override string ToString()
+        {
+            return this.Juego.Codigo + " " + this.Jugador.Codigo; 
+        }
+
+    }
+}
